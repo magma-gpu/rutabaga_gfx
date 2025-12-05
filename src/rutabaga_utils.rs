WhatAmISupposedToPutHere/libkrun

Add pipewire cross domain channel type.
This adds a pipewire channel type, increases the maximum number
of fds that can be transferred in a single message, and adds support
for sharing eventfds between the guest and host.

Signed-off-by: Sasha Finkelstein <fnkl.kernel@gmail.com>

src/rutabaga_gfx/src/rutabaga_utils.rs
@@ -599,6 +599,7 @@ impl Transfer3D {
 /// Rutabaga channel types
 pub const RUTABAGA_CHANNEL_TYPE_WAYLAND: u32 = 0x0001;
 pub const RUTABAGA_CHANNEL_TYPE_CAMERA: u32 = 0x0002;
+pub const RUTABAGA_CHANNEL_TYPE_PW: u32 = 0x0010;
 pub const RUTABAGA_CHANNEL_TYPE_X11: u32 = 0x0011;
 
 /// Information needed to open an OS-specific RutabagaConnection (TBD).  Only Linux hosts are

magma-gpu/rutabaga_gfx

Signed-off-by: Dorinda Bassey <dbassey@redhat.com>

src/rutabaga_utils.rs
@@ -608,6 +608,7 @@ impl Transfer3D {
 /// Rutabaga path types
 pub const RUTABAGA_PATH_TYPE_WAYLAND: u32 = 0x0001;
 pub const RUTABAGA_PATH_TYPE_GPU: u32 = 0x0002;
+pub const RUTABAGA_PATH_TYPE_PW: u32 = 0x0010;
 pub const RUTABAGA_PATH_TYPE_X11: u32 = 0x0011;
 
 pub type RutabagaPaths = Vec<RutabagaPath>;