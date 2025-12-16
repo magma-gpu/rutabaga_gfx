5484a4f2c853a05d2e53c0befe6ed4e865331274
Jeongik Cha <jeongik@google.com>
2025-12-16 22:33:43 +0900

    export_blob handles AHB as well
    Bug: 453989233
    Test: check if it is zero copy

--- src/handle.rs
+++ src/handle.rs
@@ -7,12 +7,30 @@ use mesa3d_util::OwnedDescriptor;
 
 use crate::rutabaga_utils::RutabagaResult;
 
+pub struct AhbInfo {
+    pub fds: Vec<OwnedDescriptor>,
+    pub metadata: Vec<u8>,
+}
+
+impl AhbInfo {
+    pub fn try_clone(&self) -> RutabagaResult<AhbInfo> {
+        let cloned_fds = self
+            .fds
+            .iter()
+            .map(|fd| fd.try_clone())
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(|_| MesaError::InvalidMesaHandle)?;
+
+        Ok(AhbInfo {
+            fds: cloned_fds,
+            metadata: self.metadata.clone(),
+        })
+    }
+}
+
 pub enum RutabagaHandle {
     MesaHandle(MesaHandle),
-    AhbInfo {
-        fds: Vec<OwnedDescriptor>,
-        metadata: Vec<u8>,
-    },
+    AhbInfo(AhbInfo),
 }
 
 impl From<MesaHandle> for RutabagaHandle {
@@ -32,6 +50,23 @@ impl TryFrom<RutabagaHandle> for MesaHandle {
     }
 }
 
+impl From<AhbInfo> for RutabagaHandle {
+    fn from(value: AhbInfo) -> Self {
+        RutabagaHandle::AhbInfo(value)
+    }
+}
+
+impl TryFrom<RutabagaHandle> for AhbInfo {
+    type Error = MesaError;
+
+    fn try_from(handle: RutabagaHandle) -> Result<Self, Self::Error> {
+        match handle {
+            RutabagaHandle::AhbInfo(h) => Ok(h),
+            _ => Err(MesaError::InvalidMesaHandle),
+        }
+    }
+}
+
 impl RutabagaHandle {
     /// Clones the RutabagaHandle, duplicating any underlying file descriptors.
     pub fn try_clone(&self) -> RutabagaResult<RutabagaHandle> {
@@ -39,18 +74,7 @@ impl RutabagaHandle {
             RutabagaHandle::MesaHandle(handle) => {
                 Ok(RutabagaHandle::MesaHandle(handle.try_clone()?))
             }
-            RutabagaHandle::AhbInfo { fds, metadata } => {
-                let cloned_fds = fds
-                    .iter()
-                    .map(|fd| fd.try_clone())
-                    .collect::<Result<Vec<_>, _>>()
-                    .map_err(|_| MesaError::InvalidMesaHandle)?;
-
-                Ok(RutabagaHandle::AhbInfo {
-                    fds: cloned_fds,
-                    metadata: metadata.clone(),
-                })
-            }
+            RutabagaHandle::AhbInfo(info) => Ok(RutabagaHandle::AhbInfo(info.try_clone()?)),
         }
     }
 
