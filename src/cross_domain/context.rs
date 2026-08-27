d7f4dd553eba6a5674f39f0a71f8bddb1c26e631
Val Packett <val@invisiblethingslab.com>
2026-08-26 23:49:30 -0300

    cross_domain: allow attach() of guest blobs with handles
    Simplify the attach implementation, removing the conditional on blob type
    and instead just always handling both fields of ContextResource.
    
    There is no functional change for existing kinds of resources, but
    guest resources with handles (CREATE_GUEST_HANDLE) won't have their
    handles thrown away anymore.
    
    Signed-off-by: Val Packett <val@invisiblethingslab.com>

--- src/cross_domain/context.rs
+++ src/cross_domain/context.rs
@@ -738,23 +738,13 @@ impl RutabagaContext for CrossDomainContext {
     }
 
     fn attach(&mut self, resource: &mut RutabagaResource) {
-        if resource.blob_mem == RUTABAGA_BLOB_MEM_GUEST {
-            self.context_resources.lock().unwrap().insert(
-                resource.resource_id,
-                ContextResource {
-                    handle: None,
-                    backing_iovecs: resource.backing_iovecs.take(),
-                },
-            );
-        } else if let Some(ref handle) = resource.handle {
-            self.context_resources.lock().unwrap().insert(
-                resource.resource_id,
-                ContextResource {
-                    handle: Some(handle.clone()),
-                    backing_iovecs: None,
-                },
-            );
-        }
+        self.context_resources.lock().unwrap().insert(
+            resource.resource_id,
+            ContextResource {
+                handle: resource.handle.clone(),
+                backing_iovecs: resource.backing_iovecs.take(),
+            },
+        );
     }
 
     fn detach(&mut self, resource: &RutabagaResource) {
